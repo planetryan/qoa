08c3b4fb0b1440393d820cdbaf9d61187485aa74
planetryan <rh123218@gmail.com>
2025-07-28 01:01:39 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // only in nightly
 
 #![feature(portable_simd)]
-#![feature(stdarch_x86_mm_shuffle)]
+#![feature(stdarch_x86_mm_shuffle)] // If you cannot compile on non x86 ISAs, comment this line out
 // #![feature(rustc_attrs)] // this is a internal compiler feature. using internal features are discouraged.
 
 pub mod instructions; // for instruction enum
